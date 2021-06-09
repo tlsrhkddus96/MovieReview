a47634b9e44f81ab27cd9e9fef6ccdffb0ee5723
tlsrhkddus96 <tlsrhkddus96@users.noreply.github.com>
2021-06-09 19:01:25 +0900

    Upload 프로젝트 적용 (3)

---
+++ src/main/java/org/zerock/mreview/dto/ReviewDTO.java
@@ -0,0 +1,37 @@
+package org.zerock.mreview.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReviewDTO {
+
+    //review num
+    private Long reviewnum;
+
+    //Movie mno
+    private Long mno;
+
+    //Member id
+    private Long mid;
+
+    //Member nickname
+    private String nickName;
+
+    //Member Email
+    private String email;
+
+    private int grade;
+
+    private String text;
+
+    private LocalDateTime regDate, modDate;
+
+}
--- src/main/java/org/zerock/mreview/entity/Review.java
+++ src/main/java/org/zerock/mreview/entity/Review.java
@@ -14,7 +14,7 @@ public class Review extends BaseEntity{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long reviewNum;
+    private Long reviewnum;
 
     @ManyToOne(fetch = FetchType.LAZY)
     private Movie movie;
@@ -25,5 +25,13 @@ public class Review extends BaseEntity{
     private int grade;
 
     private String text;
+
+    public void chageGrade(int grade){
+        this.grade = grade;
+    }
+
+    public void chageText(String text){
+        this.text = text;
+    }
 
 }
